49ec2ed0921438985980093fc8ee3e4bb99ee5a5
AnhPhix3405 <phamphi30042005102@gmail.com>
2026-03-05 15:45:13 +0700

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // import Image from "next/image";
-import Header from "@/components/layout/header";
-import QuickActionsGrid from '../../components/layout/QuickActionsGrid';
-import InvestmentBanner from "@/components/layout/InvestmentBanner";
+import Header from "../components/layout/header";
+import QuickActionsGrid from "../components/layout/QuickActionsGrid";
+import InvestmentBanner from "../components/layout/InvestmentBanner";
 export default function Home() {
   return (
     <div className="">
