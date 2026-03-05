e2248df37975144453ae17ade64fedeee81b9700
AnhPhix3405 <phamphi30042005102@gmail.com>
2026-03-05 17:32:02 +0700

    up

--- src/components/layout/LandUsePlanningMap.tsx
+++ src/components/layout/LandUsePlanningMap.tsx
@@ -1,7 +1,8 @@
 export default function LandUsePlanningMap() {
     return (
         <div className="w-full flex-col px-2">
-            <div className="w-full h-56 border border-black rounded-md mt-2 bg-center bg-cover"
+            <div className="w-full h-56 border border-black rounded-md mt-2 bg-center bg-cover
+             "
                     style={{ backgroundImage: 'url("https://media.istockphoto.com/id/2173059563/vi/vec-to/%C4%91%E1%BA%BFn-s%E1%BB%9Bm-h%C3%ACnh-%E1%BA%A3nh-tr%C3%AAn-n%E1%BB%81n-tr%E1%BA%AFng-kh%C3%B4ng-c%C3%B3-s%E1%BA%B5n-%E1%BA%A3nh.jpg?s=1024x1024&w=is&k=20&c=NItqj_jaYCsWltHw9WRV6kzmIiyEcsYrJM2LkWxOgtk=")' }}>
             </div>
             <div className="w-full mt-2">
