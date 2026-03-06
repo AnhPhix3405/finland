67d1eadd686a6e53b3b7f2e34ec7a1528bbaca12
AnhPhix3405 <phamphi30042005102@gmail.com>
2026-03-06 20:22:03 +0700

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,13 @@
 // import Image from "next/image";
-import QuickActionsGrid from "../components/layout/home/QuickActionsGrid";
-import InvestmentBanner from "../components/layout/home/InvestmentBanner";
-import LandUsePlanningMap from "../components/layout/home/LandUsePlanningMap";
-import RealEstatePriceMap from "../components/layout/home/RealEstatePriceMap";
-import StateLandValuation from "../components/layout/home/StateLandValuation";
-import PostingSupportPolicy from "../components/layout/home/PostingSupportPolicy";
-import InstructionalVideos from "../components/layout/home/InstructionalVideos";
+import Header from "../components/layout/header";
+import QuickActionsGrid from "../components/layout/QuickActionsGrid";
+import InvestmentBanner from "../components/layout/InvestmentBanner";
 export default function Home() {
   return (
     <div className="">
+      <Header />
       <QuickActionsGrid />
       <InvestmentBanner/>
-      <LandUsePlanningMap/>
-      <RealEstatePriceMap/>
-      <StateLandValuation/>
-      <PostingSupportPolicy/>
-      <InstructionalVideos/>
     </div>
   );
 }
